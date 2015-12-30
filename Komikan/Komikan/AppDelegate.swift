1da3217f30b9a999096ad9415daa768eb33cc63e
DrabWeb <sethunity@gmail.com>
2015-12-30 18:52:01 -0400

    Working on collection view. May have to make it in storyboards, though I really dont want to

--- Komikan/Komikan/AppDelegate.swift
+++ Komikan/Komikan/AppDelegate.swift
@@ -12,14 +12,41 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    
     @IBOutlet weak var titlebarVisualEffectView: NSVisualEffectView!
     
     @IBOutlet weak var backgroundVisualEffectView: NSVisualEffectView!
+    
+    @IBOutlet weak var mangaGridCollectionView: NSCollectionView!
+    @IBOutlet weak var mangaGridArray: NSArrayController!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         styleWindow();
+        
+        NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(0.1), target:self, selector: Selector("deleteTitlebarInFullscreen"), userInfo: nil, repeats:true);
+        
+        let itemPrototype : NSCollectionViewItem = NSCollectionViewItem(nibName: "KMMangaGridItem", bundle: NSBundle.mainBundle())!;
+        
+        mangaGridCollectionView.itemPrototype = itemPrototype;
+        
+        var newCoverImage : KMMangaGridItem = KMMangaGridItem();
+        newCoverImage.coverImage = NSImage(named: "NSUser");
+        mangaGridArray.addObject(newCoverImage);
+        
+        newCoverImage = KMMangaGridItem();
+        newCoverImage.coverImage = NSImage(named: "NSCaution");
+        mangaGridArray.addObject(newCoverImage);
+        
+        newCoverImage = KMMangaGridItem();
+        newCoverImage.coverImage = NSImage(named: "NSTrashEmpty");
+        mangaGridArray.addObject(newCoverImage);
+        
+        newCoverImage = KMMangaGridItem();
+        newCoverImage.coverImage = NSImage(named: "example-cover");
+        mangaGridArray.addObject(newCoverImage);
+        
+        print(mangaGridArray.arrangedObjects);
     }
     
     func styleWindow() {
@@ -30,11 +57,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         backgroundVisualEffectView.material = NSVisualEffectMaterial.Dark;
         titlebarVisualEffectView.material = NSVisualEffectMaterial.UltraDark;
     }
-
+    
+    func deleteTitlebarInFullscreen() {
+        if(window.frame == NSScreen.mainScreen()?.frame) {
+            window.toolbar?.visible = false;
+        }
+        else {
+            window.toolbar?.visible = true;
+        }
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
---
+++ Komikan/Komikan/KMWindow.swift
@@ -0,0 +1,13 @@
+//
+//  KMWindow.swift
+//  Komikan
+//
+//  Created by Seth on 2015-12-30.
+//  Copyright © 2015 DrabWeb. All rights reserved.
+//
+
+import Cocoa
+
+class KMWindow: NSWindow {
+    
+}
