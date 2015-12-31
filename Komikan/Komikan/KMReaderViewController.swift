c2c719eb5bad27629817c12ee20f1ca995173db1
DrabWeb <sethunity@gmail.com>
2015-12-31 13:59:13 -0400

    Commented KMReaderViewController

--- Komikan/Komikan/KMReaderViewController.swift
+++ Komikan/Komikan/KMReaderViewController.swift
@@ -22,49 +22,69 @@ class KMReaderViewController: NSViewController {
         // Style the window
         styleWindow();
         
+        // Start the 0.1 second loop for the mouse hovering
         NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(0.1), target:self, selector: Selector("mouseHoverHandling"), userInfo: nil, repeats:true);
     }
     
     func mouseHoverHandling() {
-        if(readerWindow.orderedIndex == 1) {
-            var insideWindow : Bool = false;
-            
-            var ourEvent : CGEventRef = CGEventCreate(nil)!;
-            var point = CGEventGetLocation(ourEvent);
-            
-            var windowFrame : NSRect! = view.window?.frame;
-            
-            var pointY = abs(point.y - NSScreen.mainScreen()!.frame.height);
-            
-            if(point.x > windowFrame.origin.x && point.x < windowFrame.origin.x + windowFrame.width) {
-                if(pointY > windowFrame.origin.y && pointY < windowFrame.origin.y + windowFrame.height) {
-                    // Inside window!
-                    insideWindow = true;
-                }
+        // A bool to say if we are hovering the window
+        var insideWindow : Bool = false;
+        
+        // Create a new CGEventRef, for the mouse position
+        var mouseEvent : CGEventRef = CGEventCreate(nil)!;
+        
+        // Get the mouse point onscreen from ourEvent
+        var mousePosition = CGEventGetLocation(mouseEvent);
+        
+        // Store the windows frame temporarly, so we dont retype a millino times
+        var windowFrame : NSRect! = readerWindow.frame;
+        
+        // Create a variable to store the cursors location y where 0 0 is the bottom left
+        var pointY = abs(mousePosition.y - NSScreen.mainScreen()!.frame.height);
+        
+        // If the mouse position is inside the window on the x...
+        if(mousePosition.x > windowFrame.origin.x && mousePosition.x < windowFrame.origin.x + windowFrame.width) {
+            // If the mouse position is inside the window on the y...
+            if(pointY > windowFrame.origin.y && pointY < windowFrame.origin.y + windowFrame.height) {
+                // The cursor is inside the window, say so
+                insideWindow = true;
             }
-            
-            if(insideWindow) {
-                fadeInTitlebar();
-            }
-            else {
-                fadeOutTitlebar();
-            }
+        }
+        
+        // If the cursor is inside the window...
+        if(insideWindow) {
+            // Fade in the titlebar
+            fadeInTitlebar();
+        }
+        // If the cursor is outside the window...
+        else {
+            // Fade out the titlebar
+            fadeOutTitlebar();
         }
     }
     
     func fadeOutTitlebar() {
+        // Use the animator to fade out the titlebars visual effect view
         titlebarVisualEffectView.animator().alphaValue = 0;
+        
+        // Use the animator to fade out the windows titlebar
         readerWindow.standardWindowButton(NSWindowButton.CloseButton)?.superview?.superview?.animator().alphaValue = 0;
     }
     
     func fadeInTitlebar() {
+        // Use the animator to fade in the titlebars visual effect view
         titlebarVisualEffectView.animator().alphaValue = 1;
+        
+        // Use the animator to fade in the windows titlebar
         readerWindow.standardWindowButton(NSWindowButton.CloseButton)?.superview?.superview?.animator().alphaValue = 1;
     }
     
     func styleWindow() {
         // Get the reader window
         readerWindow = NSApplication.sharedApplication().windows.last!;
+        
+        // Hide the titlebar
+        readerWindow.standardWindowButton(NSWindowButton.CloseButton)?.superview?.superview?.alphaValue = 0;
         
         // Set it to have a full size content view
         readerWindow.styleMask |= NSFullSizeContentViewWindowMask;
