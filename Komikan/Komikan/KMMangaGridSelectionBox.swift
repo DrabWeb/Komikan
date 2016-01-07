f2df09c91d79706a0d31005412fcc71466f1b0c6
DrabWeb <sethunity@gmail.com>
2016-01-07 16:10:42 -0400

    Added selected item highlighting to the manga grid

--- Komikan/Komikan/KMMangaGridSelectionBox.swift
+++ Komikan/Komikan/KMMangaGridSelectionBox.swift
@@ -17,8 +17,19 @@ class KMMangaGridSelectionBox: NSBox {
     }
     
     override func awakeFromNib() {
+        // Set it to be a custom box
         self.boxType = NSBoxType.Custom;
-        self.borderType = NSBorderType.LineBorder;
-        self.fillColor = NSColor.selectedControlColor();
+        
+        // Set the corner radius to 5
+        self.cornerRadius = 5;
+        
+        // Set the alpha value to 0.2
+        self.alphaValue = 0.2;
+        
+        // Set the box to have no border
+        self.borderType = NSBorderType.NoBorder;
+        
+        // Set the background color to black
+        self.fillColor = NSColor.blackColor();
     }
 }
