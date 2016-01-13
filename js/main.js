2ea6fdd211f0421db6b5cbf6a18069471d83d3da
DrabWeb <sethunity@gmail.com>
2016-01-13 18:15:34 -0400

    Litttle things I forgot to add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-$( "#screen-2" ).fadeOut();
-$( "#screen-3" ).fadeOut();
-$( "#screen-4" ).fadeOut();
-$( "#screen-5" ).fadeOut();
-$( "#screen-6" ).fadeOut();
-$( "#screen-7" ).fadeOut();
-$( "#screen-8" ).fadeOut();
+function fadeOutAll() {
+	$( "#screen-1" ).fadeOut();
+	$( "#screen-2" ).fadeOut();
+	$( "#screen-3" ).fadeOut();
+	$( "#screen-4" ).fadeOut();
+	$( "#screen-5" ).fadeOut();
+	$( "#screen-6" ).fadeOut();
+	$( "#screen-7" ).fadeOut();
+	$( "#screen-8" ).fadeOut();
+}
+
+fadeOutAll();
+$( "#screen-1" ).fadeIn();
 
 window.setTimeout(fadeFromBlack,1000);
 
@@ -12,35 +18,58 @@ function fadeFromBlack() {
 	$( "#fade-out" ).fadeOut();
 }
 
-$( "#screen-1" ).click(function() {
-  $( "#screen-1" ).fadeOut();
-  $( "#screen-2" ).fadeIn();
+function goToPage(page) {
+	fadeOutAll();
+	console.log(page);
+	$( "#screen-" + page ).fadeIn();
+}
+
+$( "#importClick" ).click(function() {
+	goToPage(4);
 });
-$( "#screen-2" ).click(function() {
-  $( "#screen-2" ).fadeOut();
-  $( "#screen-3" ).fadeIn();
+$( "#editingClick" ).click(function() {
+	goToPage(6);
+});
+$( "#tagsClick" ).click(function() {
+	goToPage(4);
+});
+$( "#bookmarksClick" ).click(function() {
+	goToPage(7);
+});
+$( "#seriesClick" ).click(function() {
+	goToPage(4);
+});
+$( "#writerClick" ).click(function() {
+	goToPage(4);
+});
+$( "#sortClick" ).click(function() {
+	goToPage(5);
+});
+$( "#searchClick" ).click(function() {
+	goToPage(5);
+});
+$( "#viewerClick" ).click(function() {
+	goToPage(3);
+});
+
+$( "#screen-1" ).click(function() {
+	goToPage(2);
 });
 $( "#screen-3" ).click(function() {
-  $( "#screen-3" ).fadeOut();
-  $( "#screen-4" ).fadeIn();
+	goToPage(4);
 });
 $( "#screen-4" ).click(function() {
-  $( "#screen-4" ).fadeOut();
-  $( "#screen-5" ).fadeIn();
+	goToPage(5);
 });
 $( "#screen-5" ).click(function() {
-  $( "#screen-5" ).fadeOut();
-  $( "#screen-6" ).fadeIn();
+	goToPage(6);
 });
 $( "#screen-6" ).click(function() {
-  $( "#screen-6" ).fadeOut();
-  $( "#screen-7" ).fadeIn();
+	goToPage(7);
 });
 $( "#screen-7" ).click(function() {
-  $( "#screen-7" ).fadeOut();
-  $( "#screen-8" ).fadeIn();
+	goToPage(8);
 });
 $( "#screen-8" ).click(function() {
-  $( "#screen-8" ).fadeOut();
-  $( "#screen-1" ).fadeIn();
+	goToPage(1);
 });
