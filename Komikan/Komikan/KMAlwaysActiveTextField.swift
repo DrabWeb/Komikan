23afe28dc50ab847f46381b43972f2e95b01eaba
DrabWeb <sethunity@gmail.com>
2016-02-26 12:08:53 -0400

    Fixed the age old bug of the search field hacing a darker box in the center when not selected

---
+++ Komikan/Komikan/KMAlwaysActiveTextField.swift
@@ -0,0 +1,24 @@
+//
+//  KMAlwaysActiveTextField.swift
+//  Komikan
+//
+//  Created by Seth on 2016-02-26.
+//  Copyright © 2016 DrabWeb. All rights reserved.
+//
+
+import Cocoa
+
+class KMAlwaysActiveTextField: NSTextField {
+    
+    // Always say we can be first responder so we always get the active look
+    override var acceptsFirstResponder : Bool {
+        return true;
+    }
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
